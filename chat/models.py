fff997213d9f48309bfae04feb3c2054496bb931
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-09-12 15:35:49 +0900

    feat : 메시지 실시간 알림 기능 추가

--- chat/models.py
+++ chat/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from notify.models import Notification
 from django.db.models.signals import post_save
+import requests
 
 User = get_user_model()
 
@@ -27,3 +28,21 @@ class Message(models.Model):
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=200,null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+def message_action(sender, **kwargs):
+    if kwargs['created']:
+        message = kwargs['instance']
+        room = message.room
+        if room.firstuser == message.writer:
+            receiver = room.seconduser
+        else:
+            receiver = room.firstuser
+        content = f'메시지를 보내셨습니다.'
+        noti = Notification.objects.create(sender=message.writer,receiver=receiver,content=content)
+        datas = {
+            'notify_id': noti.id
+        }
+        url = "http://127.0.0.1:8000/notify/massage/"
+        res = requests.post(url,data=datas)
+
+post_save.connect(message_action, sender=Message)
