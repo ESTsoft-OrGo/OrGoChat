7c8ceaed68a2bbec2a7808d1aa9dac45188b66bc
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-09-14 09:14:59 +0900

    feat : 채팅방 접속 유저 카운트 기능 개선

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,6 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from .models import Room , Message
+from .models import Room, Message
 from django.contrib.auth import get_user_model
 from django.db.models.functions import TruncDay
 
@@ -8,15 +8,15 @@ User = get_user_model()
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
-    
+
     async def connect(self):
-        
+
         user = self.scope["user"]
 
         if not user.is_authenticated:
             self.close()
-            
-    	# 파라미터 값으로 채팅 룸을 구별
+
+        # 파라미터 값으로 채팅 룸을 구별
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
@@ -27,16 +27,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
         room = Room.objects.get(title=self.room_name)
-        room.joinUser = room.joinUser + 1
-        room.save()
-        
+        if room.joinUser < 2:
+            room.joinUser = room.joinUser + 1
+            room.save()
+
         await self.accept()
 
     async def disconnect(self, close_code):
         room = Room.objects.get(title=self.room_name)
-        room.joinUser = room.joinUser - 1
-        room.save()
-        
+        if room.joinUser > 0:
+            room.joinUser = room.joinUser - 1
+            room.save()
+
         # 룸 그룹 나가기
         await self.channel_layer.group_discard(
             self.room_group_name,
@@ -48,15 +50,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         method = text_data_json['method']
         message = text_data_json['message']
-        
+
         # 룸 그룹으로 메세지 보냄
         if method == 'join':
             await self.find_room(self.room_name)
-            
+
             room = Room.objects.get(title=self.room_name)
-            join_messages = Message.objects.filter(room=room).annotate(day=TruncDay('created_at')).values('day', 'id')
+            join_messages = Message.objects.filter(room=room).annotate(
+                day=TruncDay('created_at')).values('day', 'id')
             join_message = await self.messages_to_json(join_messages)
-            
+
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
@@ -70,10 +73,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
             user = User.objects.get(pk=writer)
             room = Room.objects.get(title=self.room_name)
             if room.joinUser > 1:
-                message = Message.objects.create(writer=user,content=message,room=room,is_read=True)
+                message = Message.objects.create(
+                    writer=user, content=message, room=room, is_read=True)
             else:
-                message = Message.objects.create(writer=user,content=message,room=room)
-                
+                message = Message.objects.create(
+                    writer=user, content=message, room=room)
+
             new_message = await self.message_to_json(message)
             await self.channel_layer.group_send(
                 self.room_group_name,
@@ -87,7 +92,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # 룸 그룹 방을 찾거나 만듬
     async def find_room(self, data):
         room = Room.objects.get_or_create(title=data)
-    
+
     # 룸 그룹으로부터 메세지 받음
     async def chat_message(self, event):
         message = event['message']
@@ -97,13 +102,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'message': message,
             'status': status
         }))
-    
+
     async def messages_to_json(self, messages):
         # 날짜별로 메시지들을 그룹화
         message_grouped_by_day = {}
         for entry in messages:
             day = entry['day'].strftime('%Y.%m.%d')
-            message = Message.objects.get(id=entry['id'])  # 해당 메시지 가져오기 (필요한 필드에 맞게 변경)
+            # 해당 메시지 가져오기 (필요한 필드에 맞게 변경)
+            message = Message.objects.get(id=entry['id'])
             message.is_read = True
             message.save()
             wrtier_id = message.writer.id
@@ -117,9 +123,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 message_grouped_by_day[day] = [data]
             else:
                 message_grouped_by_day[day].append(data)
-        
+
         return message_grouped_by_day
-    
+
     async def message_to_json(self, message):
         result = []
         wrtier_id = message.writer.id
