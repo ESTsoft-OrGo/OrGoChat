23e3fa5aa9d2d5d960a424aed17987584df9ae86
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-10-02 15:01:12 +0900

    feat : 그룹챗 Consumer 추가

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
-from .consumers import ChatConsumer
+from .consumers import ChatConsumer, GroupChatConsumer
 
 websocket_urlpatterns = [
     path('chat/<str:room_name>', ChatConsumer.as_asgi()),
+    path('groupchat/<str:room_name>', GroupChatConsumer.as_asgi()),
 ]
